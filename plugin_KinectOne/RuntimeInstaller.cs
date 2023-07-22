c4c2193fab5465e15c843005069675444ae3d33c
公彦赤屋先 <kimihikoakaya@gmail.com>
2023-07-23 04:24:12 +0900

    SVG Path icon for better overall quality

--- plugin_KinectOne/RuntimeInstaller.cs
+++ plugin_KinectOne/RuntimeInstaller.cs
@@ -11,16 +11,17 @@ using Windows.Storage;
 using Amethyst.Plugins.Contract;
 using Microsoft.UI.Xaml.Controls;
 using RestSharp;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
 
 namespace plugin_KinectOne;
 
 internal class SetupData : ICoreSetupData
 {
-    public object PluginIcon => new BitmapIcon
+    public object PluginIcon => new PathIcon
     {
-        UriSource = new Uri(Path.Join(Directory.GetParent(
-                Assembly.GetExecutingAssembly().Location)!.FullName,
-            "Assets", "Resources", "icon.png"))
+        Data = (Geometry)XamlBindingHelper.ConvertValue(typeof(Geometry),
+            "M45.26,18.3V15.93H69.51V1.1H0V16H24.25v2.37H0v5.25H69.51V18.3ZM9.36,13.19A4.63,4.63,0,0,1,4.65,8.45a4.61,4.61,0,0,1,4.6-4.67,4.71,4.71,0,1,1,.11,9.41Z")
     };
 
     public string GroupName => "kinect";
